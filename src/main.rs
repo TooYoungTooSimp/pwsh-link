e2934cc02e43fd3149b6b91a9ed52f4e114c7eb9
TooYoungTooSimp <6648049+TooYoungTooSimp@users.noreply.github.com>
2020-05-26 01:29:36 +0800

    Tue, 26 May 2020 01:29:36 +0800

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+use std::env;
+use std::process::Command;
+fn main() {
+    Command::new("wt")
+        .args(vec!["-p", "PowerShell", include_str!("../conf/pscore.pth")])
+        .args(env::args().skip(2))
+        .spawn()
+        .unwrap();
+}
