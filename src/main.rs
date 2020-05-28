f805d34a4a10415328644305874789341da3df42
TooYoungTooSimp <6648049+TooYoungTooSimp@users.noreply.github.com>
2020-05-28 13:39:37 +0800

    Thu, 28 May 2020 13:39:36 +0800

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
 use std::env;
 use std::process::Command;
 fn main() {
+    let mut rest_args: Vec<String> = env::args().skip(2).collect();
+    if rest_args
+        .first()
+        .unwrap_or(&String::from("-"))
+        .chars()
+        .nth(0)
+        .unwrap()
+        != '-'
+    {
+        rest_args.insert(0, String::from("-Command"));
+    }
     Command::new("wt")
         .args(&["-p", "PowerShell", include_str!("../conf/pscore.pth")])
-        .args(env::args().skip(2))
+        .args(rest_args)
         .spawn()
         .unwrap();
 }
